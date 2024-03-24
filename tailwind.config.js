6f14073d5d0ed63822a39027b390fd0ae264e59d
zhuqingyun0510 <3399372902@qq.com>
2024-03-25 01:20:54 +0800

    模块修改完成

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:[
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './theme.config.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class'
+}
+
